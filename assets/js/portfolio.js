22faa33cec2a9991fc91f75afa33737f83731519
Nicole LaSusa <nicolelasusa@gmail.com>
2019-07-19 23:39:13 -0400

    commit

---
+++ assets/js/portfolio.js
@@ -0,0 +1,37 @@
+//Button Functions
+
+$(".fa-github").on('click', function(){
+    window.location = "https://github.com/nlasusa";    
+});
+
+$(".fa-github").on('click', function(){
+    window.location = "https://github.com/nlasusa";    
+});
+
+$(".fa-linkedin").on('click', function(){
+    window.location = "https://www.linkedin.com/in/nicole-lasusa-17a118a0/";    
+});
+
+$(".fa-address-card").on('click', function(){
+    window.location.href = "contact.html";
+});
+
+$(".fa-laptop").on('click', function(){
+    window.location.href = "Portfolio.html";
+});
+
+$("#link1").on('click', function(){
+    window.location.href = "https://nlasusa.github.io/unit-4-game/";
+});
+
+$("#link2").on('click', function(){
+    window.location.href = "https://nlasusa.github.io/TriviaGame/";
+});
+
+$("#link3").on('click', function(){
+    window.location.href = "https://nlasusa.github.io/train-schedule/";
+});
+
+$("#link4").on('click', function(){
+    window.location.href = "https://nlasusa.github.io/project1/";
+});
